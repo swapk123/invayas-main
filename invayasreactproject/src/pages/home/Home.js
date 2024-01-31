37f3ea1a471ffc33f34aa4ecd6bc0404d5140ccc
swapk@123 <swapnakochrekar@gmail.com>
2024-01-31 21:33:35 +0530

    home section edition

--- invayasreactproject/src/pages/home/Home.js
+++ invayasreactproject/src/pages/home/Home.js
@@ -1,25 +1,32 @@
 import React from "react";
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
 
 const Home = () => {
-
-const homeSectionContents={
-  homeSectionImage:"https://www.invayas.com/images/ones.png",
-  homeSectionHeading:"Complete Solution To All Kind Of Applications",
-  homeSectionDescription:"We are Software Professionals to convert your Ideas to Great Product",
-  homeSectionBtnContent:"Know More"
-}
+  const homeSectionContents = {
+    homeSectionImage: "https://www.invayas.com/images/ones.png",
+    homeSectionHeading: "Complete Solution To All Kind Of Applications",
+    homeSectionDescription:
+      "We are Software Professionals to convert your Ideas to Great Product",
+    homeSectionBtnContent: "Know More",
+  };
   return (
     <React.Fragment>
       <div className={styles.homeContainer}>
-        <div>
+
+        <div className={styles.homeSectionLeftSideImagePart}>
           <img src={homeSectionContents.homeSectionImage} alt="" />
         </div>
-        <div>
-          <h1>{homeSectionContents.homeSectionHeading}</h1>
-          <p className={styles.companyDetails}>{homeSectionContents.homeSectionDescription}</p>
-          <button type="button" className={styles.KnowMoreBtn}>{homeSectionContents.homeSectionBtnContent}</button>
+
+        <div className={styles.homeSectionRightSideContentPart}>
+          <h1 className={styles.homeSectionMainHeading}>{homeSectionContents.homeSectionHeading}</h1>
+          <p className={styles.companyDetails}>
+            {homeSectionContents.homeSectionDescription}
+          </p>
+          <button type="button" className={styles.KnowMoreBtn}>
+            {homeSectionContents.homeSectionBtnContent}
+          </button>
         </div>
+
       </div>
     </React.Fragment>
   );
